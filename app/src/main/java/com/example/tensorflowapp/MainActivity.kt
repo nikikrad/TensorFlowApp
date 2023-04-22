67f79a564f94d2173856da22c3e5df08dbdf217f
nikikrad <nikikrad228@gmail.com>
2023-04-22 16:13:48 +0300

    start

---
+++ app/src/main/java/com/example/tensorflowapp/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.tensorflowapp
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.pm.ActivityInfo
+import android.os.Build
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import androidx.core.view.forEach
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.tensorflowapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        val bottomNavigationView = binding.bnvNavigation
+        bottomNavigationView
+            .menu.forEach {
+                bottomNavigationView.findViewById<View>(it.itemId).setOnLongClickListener {
+                    true
+                }
+            }
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+}
+
